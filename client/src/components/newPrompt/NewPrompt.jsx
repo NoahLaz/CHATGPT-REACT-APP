9119cb4e74c53f4323b977eea4579d8173ddae49
nouh lazreq <nouh.salama@gmail.com>
2024-10-03 00:26:40 +0100

    added many functionalities such as Ai chat bot using gemini

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
+import UploadImage from "../upload/UploadImage";
+
 import "./newPrompt.css";
 
-const NewPrompt = () => {
+const NewPrompt = ({ setImg, getAiResponse }) => {
+  const [prompt, setPrompt] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!prompt) return;
+    getAiResponse(prompt);
+    setPrompt("");
+  };
   return (
-    <div className="newPrompt">
-      <form action="" className="promptForm">
-        <label htmlFor="file" className="attachment">
-          <img src="/attachment.png" alt="" />
-        </label>
-        <input id="file" type="file" hidden multiple={false} />
+    <>
+      <form action="" className="promptForm" onSubmit={handleSubmit}>
+        <UploadImage setImg={setImg} />
         <textarea
           placeholder="Ask me Anything..."
           name="prompt"
           rows="1"
+          value={prompt}
+          onChange={(e) => {
+            setPrompt(e.target.value);
+          }}
         ></textarea>
         <button>
           <img src="/arrow.png" alt="" />
         </button>
       </form>
-    </div>
+    </>
   );
 };
 
--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,52 +1,73 @@
-import { useRef } from "react";
 import "./chatPage.css";
-import { useEffect } from "react";
+
+import { useEffect, useRef, useState } from "react";
+import { IKImage } from "imagekitio-react";
+
+import MarkDown from "react-markdown";
+import model from "../../lib/gemini";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
 
+const chatHistory = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Hello" }],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Great to meet you. What would you like to know?" }],
+    },
+  ],
+});
+
 const ChatPage = () => {
+  const [question, setQuestion] = useState();
+  const [answer, setAnswer] = useState();
+
   const ref = useRef(null);
+
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    imgPath: "",
+    imgData: {},
+  });
+
+  const getAiResponse = async (prompt) => {
+    setQuestion(prompt);
+    const result = await chatHistory.sendMessageStream(
+      Object.entries(img.imgData).length ? [img.imgData, prompt] : [prompt]
+    );
+    let accumulatedText = "";
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      accumulatedText += chunkText;
+      setAnswer(accumulatedText);
+    }
+
+    setImg({
+      isLoading: false,
+      error: "",
+      imgPath: "",
+      imgData: {},
+    });
+  };
 
   useEffect(() => {
     ref.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
+  }, [question, answer, img]);
+
   return (
     <div className="chatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message user">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
-            quidem, illo quas nam beatae praesentium recusandae ad laudantium
-            soluta in quibusdam dolorem hic vero, sequi consequuntur dignissimos
-            aliquid officiis.
+          {question && <div className="message user">{question}</div>}
+          {answer && <div className="message bot">{answer}</div>}
+          {img?.isLoading && <div>Uploading Image...</div>}
+          <div className="gap" ref={ref} />
+          <div className="formContainer">
+            <NewPrompt setImg={setImg} getAiResponse={getAiResponse} />
           </div>
-          <div className="message bot">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatibus vitae ipsa sint culpa, error recusandae nisi alias
-            earum et possimus cupiditate sit voluptas, minus quaerat velit
-            corrupti ut eaque? Nulla.
-          </div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div className="message user">Whats 2+2</div>
-          <div className="message bot">2+2=4</div>
-          <div ref={ref} />
-        </div>
-        <div className="formContainer">
-          <NewPrompt />
         </div>
       </div>
     </div>
